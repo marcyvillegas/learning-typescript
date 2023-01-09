d1ac182de3dcb7af164e25564e8a45b19839803d
Marcy Villegas <marcyvillegas17@gmail.com>
2023-01-10 01:12:03 +0800

    updated config

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,6 @@
 "use strict";
 let age = 10;
+if (age < 50)
+    age += 5;
+console.log(age);
+//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
 let age: number = 10;
+if (age < 50)
+    age += 5;
+console.log(age);
