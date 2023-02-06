05831633c04c5d7a2f1cd4ae7bafb860203f9de0
Marcy Villegas <marcyvillegas17@gmail.com>
2023-02-06 23:05:18 +0800

    Added notes on functions

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,26 @@
+// Source: https://www.youtube.com/watch?v=d56mG7DezGs&t=2255s
+
 // DATA TYPES
 // 1. ANY
 let sales = 123_324;
-let course = "Typescript"
+let course = "Typescript";
 let isPublished = true;
 
 function render(document: any) {
-    
-    console.log(document)
+  console.log(document);
 }
 
-
 // 2. ARRAY
-let number: number[] = [1,2,4];
+let number: number[] = [1, 2, 4];
 // let number: number[] = [1,2,"4"]; <- this is invalid
 
 let letters: string[] = [];
 letters[0] = "This is a string";
 // letters[1] = 2; <- this is invalid
 
-
 // 3. TUPLE
 // Example we want to store [17, "Marcy"]
 let user: [number, string] = [17, "Marcy"];
-
 
 // 4. ENUMS
 
@@ -31,11 +29,29 @@ let user: [number, string] = [17, "Marcy"];
 // const medium = 2;
 // const large = 3;
 
-const enum Size { Small = 1, Medium, Large }; // in default, the first member is 0
+const enum Size {
+  Small = 1,
+  Medium,
+  Large,
+} // in default, the first member is 0
 let mySize: Size = Size.Large; // 3
-
 
 //------------------------------
 
 // FUNCTIONS
+// parameters have types and the return also
+// if function does not return anything, void will be the type
+// if parameters can be optional then only add ?:
+// when adding a default value to a parameter just simply add a value -> sampleFunction(arg = 1000): void {  }
 
+function calculateTax(income = 5000, job?: string): string {
+  if (income > 1000 && job === "decent job") return "nice";
+
+  return "oh noh";
+}
+
+calculateTax(10000, "decent job");
+
+//----------------------------
+
+// OBJECTS
